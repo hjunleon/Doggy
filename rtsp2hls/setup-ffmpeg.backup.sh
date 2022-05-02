b7c645f5579fe5192c962e33b8974571b6ff559c
hjunleon <randell.crapy@gmail.com>
2022-05-03 02:24:44 +0800

    RPI3B_1

--- rtsp2hls/setup-ffmpeg.backup.sh
+++ rtsp2hls/setup-ffmpeg.backup.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
-set -x #echo on
 VIDSOURCE="rtsp://192.168.50.242:8554/"
 CAMSOURCE="/dev/video0"
-AUDIOSOURCE="hw:1,0"
-AUDIO_OPTS="-f alsa -ac 1"
+AUDIO_OPTS="-c:a aac -b:a 160000 -ac 2"
 VIDEO_OPTS="-s 640x480 -framerate 10 -c:v libx264 -b:v 800000"
 OUTPUT_HLS="-r 10 -hls_time 10 -hls_list_size 10 -start_number 1"
-OUTPUT_NAME="av10FPS"
-COMMAND="ffmpeg $AUDIO_OPTS -i $AUDIOSOURCE -i $CAMSOURCE -y $VIDEO_OPTS $OUTPUT_HLS $OUTPUT_NAME.m3u8"
-
-
-
-$COMMAND
+OUTPUT_NAME="testing10FPS"
+ffmpeg -i "$CAMSOURCE" -y $AUDIO_OPTS $VIDEO_OPTS $OUTPUT_HLS $OUTPUT_NAME.m3u8
 
 
